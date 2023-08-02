138f23c75caaf4cb686146131af9ca23bd9a70a1
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-08-02 21:16:26 +0700

    API login register accesstoken

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
 
-const port = 3000;
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
+app.use(cors(corsOptions));
 
-app.listen(port, () => 
-console.log(`Server started on port ${port}`));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// database
+const db = require("./models");
+const Role = db.role;
+
+db.sequelize.sync();
+// force: true will drop the table if it already exists
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Drop and Resync Database with { force: true }');
+//   initial();
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// routes
+require('./routes/authroutes')(app);
+require('./routes/usersroutes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
---
+++ models/users.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      fullname:{
+        type: Sequelize.STRING
+      }, 
+      address:{
+        type: Sequelize.STRING
+      },
+      phone:{
+        type: Sequelize.STRING
+      }
+    });
+  
+    return User;
+  };
+  
