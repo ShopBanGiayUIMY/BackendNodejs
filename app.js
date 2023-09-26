bc8518981cbceee64100baf916a057ae7c01813c
huyfpl <huynvph20687@fpt.edu.vn>
2023-09-26 22:23:25 +0700

    auth login Register

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+let mysql = require('mysql2');
+let myConnection = require('express-myconnection');
+const authRouter = require('./routers/auth');
 const bodyParser = require('body-parser');
-
-const port = 3000;
+dotenv.config();// sài .env
 const app = express();
+app.use(cors());
+app.use(cookieParser());// tạo cookie 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+// Kết nối
+app.use(myConnection(mysql, {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    connectTimeout: process.env.DB_CONNECT_TIMEOUT
+}, 'single'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// Router
+app.use('/api/v1/auth',authRouter);
+// app.use('/api/v1/user',userRouter);
 
-
-app.listen(port, () => 
-console.log(`Server started on port ${port}`));
+  app.listen( process.env.PORT, () => console.log('Server is running on port 3000'));
---
+++ controllers/authcontroller.js
@@ -0,0 +1,128 @@
+
+const bcrypt = require('bcrypt'); // hash password
+const jwt = require('jsonwebtoken'); // tạo token
+const Queryuser = require('../Querydb/Queryuser');
+// generate token
+let refreshTokens = [];
+const authController = {
+    // register
+    registerUser: async (req, res) => {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            // create user
+            req.getConnection((err, conn) => {
+              const kq =  conn.query(Queryuser.registerUser, {
+                    username: req.body.username,
+                    password: hashedPassword,
+                }, (err, result) => {
+                    if (err) {
+                        return res.status(500).json({ error: 'Error registering the user'});
+                    }
+                    res.status(200).json(result);
+                });
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Error registering the user' });
+        }
+    },
+    generateAccessToken: (user) => {
+        return jwt.sign({
+            id: user.id,
+            admin: user.admin,
+        }, process.env.JWT_ACCESS_KEY, { expiresIn: '200s' });
+    },
+    generateRefreshToken: (user) => {
+        return jwt.sign({
+            id: user.id,
+            admin: user.admin,
+        }, process.env.JWT_REFRESH_KEY, { expiresIn: '365d' });
+    },
+
+    // login
+    loginUser: async (req, res) => {
+        try {
+            // Kiểm tra username có tồn tại trong database hay không
+            req.getConnection((err, conn) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Error connecting to database' });
+                }
+                conn.query(Queryuser.loginUser, req.body.username, async (err, result) => {
+                    if (err) {
+                        return res.status(500).json({ error: 'Error logging in' });
+                    }
+                    if (result.length === 0) {
+                        return res.status(404).json({ error: 'User not found' });
+                    }
+                    const user = result[0];
+    
+                    // So sánh password nhập vào và password trong database
+                    const validPassword = await bcrypt.compare(req.body.password, user.password);
+                    if (!validPassword) {
+                        return res.status(400).json({ error: 'Wrong password' });
+                    }
+    
+                    // Tạo AccessToken
+                    const accesstoken = authController.generateAccessToken(user);
+    
+                    // Tạo RefreshToken
+                    const refreshtoken = authController.generateRefreshToken(user);
+    
+                    // Lưu RefreshToken vào mảng (nếu cần)
+                    refreshTokens.push(refreshtoken);
+    
+                    // Gửi AccessToken và thông tin người dùng (loại bỏ mật khẩu) cho client
+                    const { password, ...info } = user;
+                    res.cookie('RefreshToken', refreshtoken, {
+                        httpOnly: true,
+                        path: "/",
+                        sameSite: "strict",
+                        secure: false,
+                    });
+                    res.status(200).json({ ...info, accesstoken });
+                });
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error logging in' });
+        }
+    },
+    
+    // tạo accesstoken mới từ refreshtoken
+    refreshToken: async (req, res) => {
+        const rf_token = req.cookies.RefreshToken;
+         if (!rf_token)
+            return res.status(401).json("Bạn chưa đăng nhập");
+         if (!refreshTokens.includes(rf_token))
+         // kiểm tra refreshtoken có trong mảng refreshTokens hay không     
+        jwt.verify(rf_token, process.env.JWT_REFRESH_KEY, (err, user) => {
+            if (err) {
+               console.log(err);
+            }
+              // xóa refreshtoken cũ trong mảng refreshTokens
+             refreshTokens = refreshTokens.filter((token) => token !== rf_token);
+           // tạo accesstoken mới và refreshtoken mới
+             const newAccessToken = authController.generateAccessToken(user);
+             const newRefreshToken = authController.generateRefreshToken(user);
+             refreshTokens.push(newRefreshToken);// thêm refreshtoken mới vào mảng refreshTokens
+            res.cookie('RefreshToken', newRefreshToken, {
+                httpOnly: true,
+                path: "/",
+                sameSite: "strict",
+                secure: false,
+            });
+            res.status(200).json({accesstoken: newAccessToken});
+        
+        });
+    },
+    // logout
+    logoutUser: async (req, res) => {
+        res.clearCookie('RefreshToken');
+        refreshTokens = refreshTokens.filter((token) => token !== req.cookies.RefreshToken);
+        res.status(200).json("Đăng xuất thành công");
+    },
+
+};
+
+module.exports = authController;
