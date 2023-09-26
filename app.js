40e5fd03c7a6a3c82a1f962858bfd53b10666f7c
MinhTran <tminh400@gmail.com>
2023-09-26 22:25:46 +0700

    add connection to mysql

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import connection from "./src/config/Connection.js";
 
 const port = 3000;
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => 
-console.log(`Server started on port ${port}`));
+app.get('/', (req, res) => {
+  const db = connection();
+  db.connect();
+  db.query('SELECT * FROM products', async (err, rows, fields) => {
+    if (err) {
+      res.send({ error: err }).status(500);
+      throw err;
+    } 
+    res.send(rows);
+  })
+});
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- src/config/Connection.js
+++ src/config/Connection.js
@@ -3,7 +3,6 @@ import {config} from 'dotenv';
 
 config();
 const connection = () => {
-  console.log(process.env.DATABASE_HOST, process.env.DATABASE_PORT, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, process.env.DATABASE_NAME)
   return createConnection({
       host: process.env.DATABASE_HOST,
       port: process.env.DATABASE_PORT,
