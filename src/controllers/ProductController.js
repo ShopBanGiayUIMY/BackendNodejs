8012763695a97bbd81bf93470d01a0168a073fc4
MinhTran <tminh400@gmail.com>
2023-09-28 04:45:04 +0700

    Update ProductController.js

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,8 +5,8 @@ const ProductController = {
     db.connect();
     db.query('SELECT * FROM products', async (err, rows, fields) => {
       if (err) {
-        res.send({ error: err }).status(500);
-        throw err;
+        res.status(500).send({ error: err });
+        return;
       }
       const data = rows.map((row) => {
         return {
@@ -25,10 +25,16 @@ const ProductController = {
   show: async (req, res) => {
     const db = connection();
     db.connect();
-    db.query(`select * from products p JOIN product_image i on p.id = i.product_id where p.id = ${req.params.id}`, async (err, rows, fields) => {
+    const id = req.params.id;
+    const query = 'SELECT p.*, i.image_url FROM products p JOIN product_image i ON p.id = i.product_id WHERE p.id = ?';
+    db.query(query, id, async (err, rows) => {
       if (err) {
-        res.send({ error: err }).status(500);
-        throw err;
+        res.status(500).send({ error: 'server error' });
+        return;
+      }
+      if (rows.length === 0) {
+        res.status(404).send({ message: `Not found product with id = ${req.params.id}` });
+        return;
       }
       const resBody = {
           id: rows[0].id,
@@ -40,7 +46,7 @@ const ProductController = {
           images: rows.map(row => row.image_url),
         }
       // console.log(data);
-      res.send(resBody);
+      res.status(200).send(resBody);
     })
   }
 };
