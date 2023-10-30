ff80ceb8e1ad9d34e5d7f0386c27b5e4b9a9eedd
MinhTran <tminh400@gmail.com>
2023-10-31 02:45:52 +0700

    Create Sequelize.js
    config sequelize

---
+++ config/Sequelize.js
@@ -0,0 +1,14 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    dialect: process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
+
+export default sequelize;
