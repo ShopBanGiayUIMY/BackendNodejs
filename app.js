e65ca22f3a42102078881d6f340587b599a74120
MinhTran <tminh400@gmail.com>
2023-10-31 16:46:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ProductRouter from "./routes/Products.js";
+import CartRouter from "./routes/Cart.js";
 import logger from "./middleware/Logger.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import myConnection from "express-myconnection";
 import mysql from "mysql2";
-import Routers from "./src/routes/Routers.js";
-import authRouter from "./routes/auth.js";
+import Routers from "./routes/Routers.js";
 dotenv.config();
 
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger);
+app.use('/api/v1/carts', CartRouter)
 app.use('/api/v1/products', ProductRouter);
 
 app.use(express.json());
@@ -32,11 +33,10 @@ app.use(myConnection(mysql, {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    connectTimeout: process.env.DB_CONNECT_TIMEOUT
+    database: process.env.DB_DATABASE
 }, 'single'));
 
 // Router
 app.use('/api/v1',Routers);
 //  app.listen(port, () => console.log(`Server started on port ${port}`));
- app.listen(port,"192.168.1.12", () => console.log(`Server started on port ${port}`));  
+ app.listen(process.env.SERVER_PORT,process.env.HOST_NAME, () => console.log(`Server started on port ${port}`));  
