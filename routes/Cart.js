d52d7661757595fa895be5d78d0259bce6c84c5a
MinhTran <tminh400@gmail.com>
2023-10-31 16:46:14 +0700

    Create Cart.js
    test cart route

---
+++ routes/Cart.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { CartsController } from '../controllers/CartController.js';
+const router = express.Router();
+
+// GET /carts
+router.get('/', CartsController.index);
+
+// GET /carts/:id
+router.get('/:id', (req, res) => {
+  res.send('GET /carts/:id')
+  // Return a specific cart by ID
+});
+
+// POST /carts
+router.post('/', (req, res) => {
+  res.send('POST /carts')
+  // Create a new cart
+});
+
+// PUT /carts/:id
+router.put('/:id', (req, res) => {
+  res.send('PUT /carts/:id')
+  // Update a specific cart by ID
+});
+
+// DELETE /carts/:id
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /carts/:id')
+  // Delete a specific cart by ID
+});
+
+export default router;
