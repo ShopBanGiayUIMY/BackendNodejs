6be552aa5a0771bc4e066fe569d74375085f4958
huyfpl <huynvph20687@fpt.edu.vn>
2023-11-10 12:36:36 +0700

    update

---
+++ src/services/ProductService.js
@@ -0,0 +1,20 @@
+import Product from "../models/Product.js"
+
+export const ProductService = {
+  getListProduct: async () => {
+    try {
+      const result = await Product.findAll();
+      return result;
+    } catch (e) {
+      throw e.message
+    }
+  },
+  getProductById: async (productId) => {
+    try {
+      const result = await Product.findByPk(productId);
+      return result;
+    } catch (e) {
+      throw e.message;
+    }
+  }
+}                              
