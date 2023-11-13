3100c25ac6b8f05b076a6020f734147cfba0ea3d
MinhTran <tminh400@gmail.com>
2023-11-14 00:32:45 +0700

    Update ProductService.js

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,5 +1,5 @@
 import Product from "../models/Product.js"
-
+import ProductImage from "../models/ProductImage.js"
 export const ProductService = {
   getListProduct: async () => {
     try {
@@ -11,7 +11,12 @@ export const ProductService = {
   },
   getProductById: async (productId) => {
     try {
-      const result = await Product.findByPk(productId);
+      const result = await Product.findByPk(productId, {
+        include: [{
+          model: ProductImage,
+          attributes: ["image_url"]
+        }]
+      });
       return result;
     } catch (e) {
       throw e.message;
