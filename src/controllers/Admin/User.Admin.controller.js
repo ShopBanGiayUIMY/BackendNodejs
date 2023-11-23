69a2591702319dcdadffc4f18e26edfd83899b73
thanhdao <daoptph23912@fpt.edu.vn>
2023-11-23 16:02:24 +0700

    update_second

--- src/controllers/Admin/User.Admin.controller.js
+++ src/controllers/Admin/User.Admin.controller.js
@@ -1,7 +1,37 @@
+// const layout = "layouts/layout";
+// const UserAdminController = {
+//   index: async (req, res) => {
+//     res.render("user/user", { layout: layout, title: "User" });
+//   },
+// };
+// export default UserAdminController;
+import UserService from "../../services/UserService.js";
 const layout = "layouts/layout";
+
 const UserAdminController = {
   index: async (req, res) => {
-    res.render("user/user", { layout: layout, title: "User" });
+    // Lấy danh sách người dùng
+    try {
+      const userList = await UserService.getListUser();
+
+      const data = userList.map((user) => {
+        return {
+          id: user.user_id,
+          username: user.username,
+          email: user.email,
+          phone: user.phone,
+          full_name: user.full_name,
+          address: user.address,
+        };
+      });
+
+      res.render("User/user", { data, layout: layout, title: "Người dùng" });
+    } catch (error) {
+      console.error("Lỗi khi lấy danh sách người dùng:", error);
+      res.status(500).send("Lỗi máy chủ nội bộ");
+    }
   },
+  // Thêm các phương thức khác như create, edit, update tương tự như ProductController
 };
+
 export default UserAdminController;
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,12 +1,64 @@
-import User from '../models/User.js';
+// import User from '../models/User.js';
+// const UserService = {
+//     getListUser: async () => {
+//         try {
+//             const result = await User.findAll();
+//             return result;
+//         } catch (e) {
+//             throw e.message;
+//         }
+//     },
+// }
+// export default UserService;
+import User from "../models/User.js";
+
 const UserService = {
-    getListUser: async () => {
-        try {
-            const result = await User.findAll();
-            return result;
-        } catch (e) {
-            throw e.message;
-        }
-    },
-}
+  getListUser: async () => {
+    try {
+      const result = await User.findAll();
+      return result;
+    } catch (e) {
+      throw e.message;
+    }
+  },
+
+  // Thêm chức năng lấy danh sách sản phẩm của người dùng
+  getUserProducts: async (userId) => {
+    try {
+      const user = await User.findByPk(userId, { include: "products" });
+      return user.products;
+    } catch (e) {
+      throw e.message;
+    }
+  },
+
+  // Thêm chức năng sửa thông tin người dùng
+  updateUserDetails: async (userId, updatedDetails) => {
+    try {
+      const user = await User.findByPk(userId);
+      if (user) {
+        await user.update(updatedDetails);
+        return true;
+      }
+      return false;
+    } catch (e) {
+      throw e.message;
+    }
+  },
+
+  // Thêm chức năng xóa người dùng
+  deleteUser: async (userId) => {
+    try {
+      const user = await User.findByPk(userId);
+      if (user) {
+        await user.destroy();
+        return true;
+      }
+      return false;
+    } catch (e) {
+      throw e.message;
+    }
+  },
+};
+
 export default UserService;
