7237c2bb94a4125733717727049b1feb19122296
huyfpl <huynvph20687@fpt.edu.vn>
2023-11-24 21:17:03 +0700

    update cart

--- src/routes/UserRouter/auth.js
+++ src/routes/UserRouter/auth.js
@@ -8,7 +8,7 @@ router.post('/login',authController.loginUser);
 router.post('/refresh_token',authController.refreshToken);
 router.post('/logout',middwarecontroller.verifyToken, authController.logoutUser);
 router.get('/verify/:token',authController.verifyUser);
-router.get('/infouser/:id',middwarecontroller.verifyUser, authController.GetEmailOrPhone);
+
 router.post('/verify-user/:id',middwarecontroller.verifyUser, authController.ResetPassword);
 router.post('/authentication-otp/:id',middwarecontroller.verifyUser,authController.authenticationOTP, authController.UpdatePassword);
 
