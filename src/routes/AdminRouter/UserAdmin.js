55b37078dfcc3379e38b773bad3e75d9c982f615
thanhdao <daoptph23912@fpt.edu.vn>
2023-11-24 23:17:54 +0700

    update

--- src/routes/AdminRouter/UserAdmin.js
+++ src/routes/AdminRouter/UserAdmin.js
@@ -1,9 +1,3 @@
-// import express from "express";
-// import UserAdminController from "../../controllers/Admin/User.Admin.controller.js";
-// const router = express.Router();
-// router.get("", UserAdminController.index);
-
-// export default router;
 import express from "express";
 import UserAdminController from "../../controllers/Admin/User.Admin.controller.js";
 
