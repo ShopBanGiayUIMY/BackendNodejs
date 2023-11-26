db13f64a9f7d2467ec9d449461868468f9d04c31
MinhTran <tminh400@gmail.com>
2023-11-27 04:44:10 +0700

    Update OrderService.js
    mock get orders of user Service
    prepare for API manage order for user

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,11 +1,18 @@
+import Order from '../models/Order.js'
+
 export const OrderService = {
   //needed authn
   getOrderOfUser: async (userId) => {
-    
+    const result = await Order.findAll({
+      where: {
+        userId: userId,
+      }
+    })
+    return result
   },
   //needed authn
   createOrder: async () => {
-
+    
   },
   //needed authn
   cancelOrder: async () => {
