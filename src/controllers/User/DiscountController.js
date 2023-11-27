fd05b84a1840d9dd6f562d3a9fdf50e04ee9a278
huyfpl <huynvph20687@fpt.edu.vn>
2023-11-28 02:13:16 +0700

    cập nhật voucher
    Đã thêm api tìm kiếm voucher và thêm vào giỏ của từng user

--- src/controllers/User/DiscountController.js
+++ src/controllers/User/DiscountController.js
@@ -3,10 +3,19 @@ import AuthUser from "../../models/auth.model.js";
 const VoucherController = {
   index: async (req, res) => {
     try {
-      console.log(req.user.id);
       if (req.user.id) {
         const result = await VoucherService.getListVoucher(req.user.id);
         const kq = result.map((item) => {
+          let usage_remaining1;
+          if (item.use_history == null){
+            usage_remaining1 = item.usage_quantity;
+          }else if(item.usage_quantity == 0){
+
+            usage_remaining1 = 1- (JSON.parse(item.use_history).length);
+          }
+          else{
+            usage_remaining1 = item.usage_quantity - (JSON.parse(item.use_history).length);
+          }
           return {
             voucher_id: item.voucher_id,
             voucher_name: item.voucher_name,
@@ -19,7 +28,9 @@ const VoucherController = {
             discount_amount: item.discount_amount,
             max_price: item.max_price,
             voucher_purpose: item.voucher_purpose,
+            usage_remaining:usage_remaining1,
           };
+         
         });
         res.status(200).json(kq);
       }
@@ -46,28 +57,21 @@ const VoucherController = {
               return next("Mysql error, check your query");
             }
             if (rows.length > 0) {
-              const userIdList = JSON.parse(rows[0].use_history);
+              const use_history_user = JSON.parse(rows[0].use_history);
+              const userIdList = JSON.parse(rows[0].item_user_id_list);
               const usage_quantity = JSON.parse(rows[0].usage_quantity);
-              if (userIdList != null && userIdList.length > 0) {
-                if (userIdList.includes(req.user.id)) {
-                  res.status(200).json({
-                    message: "Bạn đã sử dụng voucher này rồi",success: true
-                  });
-                } else if (
-                  rows[0].voucher_purpose == 0 &&
-                  user_item.role == 0 &&
-                  !userIdList.includes(req.user.id)
-                ) {
-                  console.log("usage_quantity");
-                  if (usage_quantity == userIdList.length) {
-                    res.status(200).json({
-                      message: "Số lượng voucher đã hết hoặc đã hết hạn",success: true
-                    });
-                  } else {
-                    conn.query(
+
+              if (use_history_user == null) {
+                if (userIdList == null) {
+                  if (rows[0].voucher_purpose == 0 && user_item.role == 0) {
+                    const result = conn.query(
                       `UPDATE vouchers
-                    SET item_user_id_list = JSON_ARRAY_APPEND(item_user_id_list, '$', ?)
-                    WHERE voucher_code = ?;`,
+                      SET item_user_id_list = JSON_ARRAY_APPEND(
+                        COALESCE(item_user_id_list, JSON_ARRAY()),
+                        '$',?
+                      )
+                      WHERE voucher_code = ?;
+                      `,
                       [req.user.id, req.body.voucher_code],
                       (err, rows) => {
                         if (err) {
@@ -75,41 +79,83 @@ const VoucherController = {
                           return next("Mysql error, check your query");
                         }
                         res.status(200).json({
-                          message: "Đã thêm voucher vào giỏ của bạn ",success: true
+                          message: "Đã thêm voucher vào giỏ của bạn",
+                          success: true,
+                        });
+                      }
+                    );
+                  }
+                } else {
+                  if (rows[0].voucher_purpose == 0 && user_item.role == 0) {
+                    if (!userIdList.includes(req.user.id)) {
+                      const result = conn.query(
+                        `UPDATE vouchers
+                        SET item_user_id_list = JSON_ARRAY_APPEND(
+                          COALESCE(item_user_id_list, JSON_ARRAY()),
+                          '$',?
+                        )
+                        WHERE voucher_code = ?;
+                        `,
+                        [req.user.id, req.body.voucher_code],
+                        (err, rows) => {
+                          if (err) {
+                            console.log(err);
+                            return next("Mysql error, check your query");
+                          }
+                          res.status(200).json({
+                            message: "Đã thêm voucher vào giỏ của bạn",
+                            success: true,
+                          });
+                        }
+                      );
+                    }else{
+                      res.status(200).json({
+                        message: "Voucher đã tồn tại trong giỏ của bạn",
+                        success: true,
+                      });
+                    }
+                  }
+                }
+              } else if (use_history_user != null) {
+                if (rows[0].voucher_purpose == 0 && user_item.role == 0) {
+                  if (use_history_user.includes(req.user.id)) {
+                    res.status(200).json({
+                      message: "Bạn đã sử dụng voucher này rồi",
+                      success: true,
+                    });
+                  } else if (userIdList.includes(req.user.id)) {
+                    res.status(200).json({
+                      message: "Voucher đã tồn tại trong giỏ của bạn",
+                      success: true,
+                    });
+                  } else if (usage_quantity == use_history_user.length) {
+                    res.status(200).json({
+                      message: "Số lượng voucher đã hết hoặc đã hết hạn",
+                      success: true,
+                    });
+                  } else if (
+                    !use_history_user.includes(req.user.id) &&
+                    !userIdList.includes(req.user.id)
+                  ) {
+                    conn.query(
+                      `UPDATE vouchers
+                      SET item_user_id_list = JSON_ARRAY_APPEND(item_user_id_list, '$', ?)
+                      WHERE voucher_code = ?;`,
+                      [req.user.id, req.body.voucher_code],
+                      (err, rows) => {
+                        if (err) {
+                          console.log(err);
+                          return next("Mysql error, check your query");
+                        }
+                        res.status(200).json({
+                          message: "Đã thêm voucher vào giỏ của bạn ",
+                          success: true,
                         });
                       }
                     );
                   }
                 }
-              } else if (userIdList == null || userIdList.length == 0) {
-                if (rows[0].voucher_purpose == user_item.role) {
-                  const result = conn.query(
-                    `UPDATE vouchers
-                    SET item_user_id_list = JSON_ARRAY_APPEND(
-                      COALESCE(item_user_id_list, JSON_ARRAY()),
-                      '$',
-                      ?
-                    )
-                    WHERE voucher_code = ?;
-                    `,
-                    [req.user.id, req.body.voucher_code],
-                    (err, rows) => {
-                      if (err) {
-                        console.log(err);
-                        return next("Mysql error, check your query");
-                      }
-                      res.status(200).json({
-                        message:
-                          "Đã thêm voucher vào giỏ của bạn khi list null",success:true
-                      });
-                    }
-                  );
-                }
               }
-            } else {
-              res.status(200).json({
-                message: "Voucher không tồn tại",success: true
-              });
             }
           }
         );
--- src/services/VoucherService.js
+++ src/services/VoucherService.js
@@ -1,7 +1,7 @@
 import Discount from "../models/Voucher.js";
 import AuthUser from "../models/auth.model.js";
-import { Op } from "sequelize";
-import sequelize from "../Connection/Sequelize.js";
+
+import connection from "../config/Connection.js";
 const VoucherService = {
   createVoucher: async (voucher) => {
     try {
@@ -50,68 +50,53 @@ const VoucherService = {
   },
   getListVoucherAdmin: async () => {
     try {
-      const result = await Discount.findAll( );
+      const result = await Discount.findAll();
       return result;
     } catch (e) {
       throw e.message;
     }
   },
 
-  getListVoucher: async (value) => {
+  getListVoucher: async (user_id) => {
+    const db = connection();
+    db.connect();
     try {
-      const [user, vouchers] = await Promise.all([
-        AuthUser.findOne({
-          where: {
-            user_id: value,
-          },
-          attributes: ["role"],
-        }),
-        Discount.findAll(),
-      ]);
-      if (user.dataValues.role == 1) {
-        return vouchers;
-        // 0 là bình thường, 1 là chào mừng
-      } else if (user.dataValues.role == 0) {
-        const result = vouchers.filter((voucher) => {
-          const userIdList = JSON.parse(voucher.item_user_id_list);
-          const use_history = JSON.parse(voucher.use_history);
-          const voucher_purpose = JSON.parse(voucher.voucher_purpose);
-          const usage_quantity = JSON.parse(voucher.usage_quantity);
-          const item_user_id_list = JSON.parse(voucher.item_user_id_list);
-          // console.log("userIdList", userIdList);
-          // console.log("use_history", use_history);
-          // console.log("voucher_purpose", voucher_purpose);
-          // console.log("usage_quantity", usage_quantity);
-          // console.log("item_user_id_list", item_user_id_list);
-          // console.log("value", value);
-          if (use_history == null) {
-            if (userIdList.includes(value)) {
-              if (voucher_purpose == 0) {
-                return voucher;
-              }
-              if (voucher_purpose == 1) {
-                return voucher;
-              }
-              if (
-                voucher_purpose == 2 &&
-                usage_quantity > item_user_id_list.length
-              ) {
-                return voucher;
-              }
-            }
-          } else {
-            if (userIdList.includes(value) || !use_history.includes(value)) {
-              if (voucher_purpose == 0 && usage_quantity > use_history.length) {
-                return voucher;
-              }
-              if (voucher_purpose == 2) {
-                return voucher;
-              }
-            }
-          }
-        });
+      const user = await AuthUser.findOne({
+        where: {
+          user_id: user_id,
+        },
+        attributes: ["role"],
+      });
+      const voucher = await Discount.findAll();
+      console.log("user.dataValues.role", user.dataValues.role);
 
-        return result;
+      if (user.dataValues.role == 0) {
+        return new Promise((resolve, reject) => {
+          db.query(
+            `
+            SELECT *
+FROM vouchers
+WHERE (voucher_purpose = 0 OR voucher_purpose = 1)
+  AND (
+    JSON_SEARCH(item_user_id_list, 'one', ?) IS NOT NULL
+    AND (
+      use_history IS NULL
+      OR JSON_SEARCH(use_history, 'one', ?) IS NULL
+    )
+  );`,
+            [user_id,user_id],
+            (err, rows) => {
+              if (err) {
+                console.log(err);
+                reject(err);
+              } else if (rows.length > 0) {
+                resolve(rows);
+              } else {
+                resolve([]);
+              }
+            }
+          );
+        });
       }
     } catch (e) {
       throw e.message;
