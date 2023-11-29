290788b27209790a0044cdbd06ebfaa53572b9b8
MinhTran <tminh400@gmail.com>
2023-11-29 13:53:59 +0700

    Update OrderService.js

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,4 +1,9 @@
+import { Sequelize } from 'sequelize';
+import CartItem from '../models/CartItem.js';
 import Order from '../models/Order.js'
+import OrderDetail from '../models/OrderDetail.js';
+import Product from '../models/Product.js';
+import ProductDetail from '../models/ProductDetail.js';
 
 export const OrderService = {
   //needed authn
@@ -6,13 +11,45 @@ export const OrderService = {
     const result = await Order.findAll({
       where: {
         userId: userId,
-      }
+      },
+      include: [
+      ]
     })
     return result
   },
   //needed authn
-  createOrder: async () => {
-    
+  createOrder: async ({
+    userId,
+    shippingAddressId,
+    paymentMethodId
+  }, items) => {
+    //TODO: should be validate data
+    const productDetails = await CartItem.findAll({
+      where: Sequelize.and(
+        {
+          cart_id: 206
+        },
+        Sequelize.or(
+          {
+            product_detail_id: [60, 61]
+          }
+        )
+      ),
+      include: {
+        model: ProductDetail,
+        attributes: null,
+        include: {
+          model: Product,
+          attributes: ["product_price"]
+        }
+      }
+    })
+    console.log(JSON.stringify(productDetails))
+    CartItem.findAll()
+    items.forEach(element => {
+      console.log(element.itemId)
+      console.log(element.cartId)
+    });
   },
   //needed authn
   cancelOrder: async () => {
