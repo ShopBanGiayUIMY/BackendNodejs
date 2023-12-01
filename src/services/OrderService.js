ba0abd9c575c4240ecedb0973dca06eab50198bb
MinhTran <tminh400@gmail.com>
2023-12-02 05:16:45 +0700

    Update OrderService.js
    add logic in service

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -4,6 +4,7 @@ import Order from '../models/Order.js'
 import OrderDetail from '../models/OrderDetail.js';
 import Product from '../models/Product.js';
 import ProductDetail from '../models/ProductDetail.js';
+import sequelize from '../Connection/Sequelize.js';
 
 export const OrderService = {
   //needed authn
@@ -13,43 +14,98 @@ export const OrderService = {
         userId: userId,
       },
       include: [
+        OrderDetail
       ]
     })
     return result
   },
-  //needed authn
-  createOrder: async ({
+  createOrderFromCart: async ({
     userId,
     shippingAddressId,
-    paymentMethodId
+    paymentMethodId,
+    cartId
   }, items) => {
     //TODO: should be validate data
-    const productDetails = await CartItem.findAll({
+    const cartItemInOrder = await CartItem.findAll({
       where: Sequelize.and(
         {
-          cart_id: 206
+          cart_id: cartId
         },
         Sequelize.or(
           {
-            product_detail_id: [60, 61]
+            item_id: items
           }
         )
       ),
       include: {
         model: ProductDetail,
-        attributes: null,
+        attributes: ["product_id", "stock"],
         include: {
           model: Product,
           attributes: ["product_price"]
         }
       }
     })
-    console.log(JSON.stringify(productDetails))
-    CartItem.findAll()
-    items.forEach(element => {
-      console.log(element.itemId)
-      console.log(element.cartId)
-    });
+    const cartItemIdInOrder = await cartItemInOrder.map(e => e.item_id)
+    if (cartItemInOrder.length !== items.length) {
+      const cartItem = await items.filter(async e => ! {cartItemId: await cartItemIdInOrder.includes(e)})
+      return {
+        status: 400,
+        message: `cart items does not belong to User ${userId}`,
+        data: cartItem
+      }
+    }
+    const error = { data: [] };
+    let totalAmount = 0;
+    for (const e of cartItemInOrder) {
+      e.canOrder = true;
+      if (e.quantity > e.ProductDetail.stock) {
+        error.data.push({ cartItemId: e.item_id });
+        e.canOrder = false;
+      }
+      e.amount = e.quantity * e.ProductDetail.Product.product_price;
+      totalAmount += e.amount;
+    }
+    if (error.data.length > 0) {
+      error.status = 401;
+      error.message = 'cannot create order with item quantity not available'
+      return error;
+    } else {
+      const transaction = await sequelize.transaction();
+      try {
+        const order = await Order.create({
+          userId: userId,
+          shippingAddressId: shippingAddressId,
+          paymentMethodId: paymentMethodId,
+          statusId: 1,
+          totalAmount: totalAmount,
+          orderDate: new Date
+        }, {transaction: transaction})
+        for (const e of cartItemInOrder) {
+          await OrderDetail.create(
+            {
+              orderId: order.id,
+              productDetailId: e.product_detail_id,
+              quantity: e.quantity,
+              price: e.ProductDetail.Product.product_price,
+            },
+            { transaction: transaction }
+          );
+          await CartItem.destroy({
+            where:{
+              item_id: e.item_id
+            }
+          })
+        }
+        await transaction.commit();
+        return {
+          status: 200,
+          message: "ok"
+        }
+      } catch (e) {
+        await transaction.rollback();
+      }
+    }
   },
   //needed authn
   cancelOrder: async () => {
